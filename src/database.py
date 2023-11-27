2730155fd1491c4a1956e61eea8c4a232844c402
nikita-th <97900107+nikita-th@users.noreply.github.com>
2023-11-27 16:27:36 +0530

    Update database.py

--- src/database.py
+++ src/database.py
@@ -7,6 +7,8 @@ db_password = 'devops123'
 db_name = 'ctg_devops_ktracker'
 db_host = 'localhost'
 
+GITHUB_TOKEN = "ghp_5354567890abcdef01234567890abcdef"
+
 db_url = f"postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}"
 db = SQLAlchemy()
 
